daab1b19b0195aee4351caf72b70a38f3c6fb7ca
dinh.truon <dinh.kttn.neu@gmail.com>
2019-03-17 10:53:46 +0700

    tạo 1 mảng ẩn để có thể tính toán, lưu trữ kết quả trên đó;thêm chặn đáy để nó sinh ra thêm hình vẽ( vuông) mới.

--- game.js
+++ game.js
@@ -2,6 +2,33 @@ let pen=canvas1.getContext("2d");
 let a=[]//Mảng của các pixcel
 let a1=[]//Mảng của các hình vuông
 let vt=0;
+let an= {
+    mangan :[],
+    maxday : [],
+    maxleft : [],
+    maxright : [],
+    bandau:function() {
+        for (let i = 0; i < 18; i++) {
+            an.mangan[i] = [];
+            an.maxday[i] = 18;
+            for (let j = 0; j < 10; j++) {
+                an.mangan[i][j] = 0
+            }
+        }
+    },
+    setmaxday:function () {
+        for(let j=0;j<10;j++){
+            for (let i=0;i<18;i++){
+                if(an.mangan[i][j]===1){
+                    an.maxday[j]=i;
+                    break;
+                }
+            }
+        }
+
+    }
+};
+
 function Pixcel(x,y,collorr) {
     this.x=x
     this.y=y
@@ -22,59 +49,68 @@ function vuong(x,y) {
     // this.collorr="red";
     this.x=x;
     this.y=y;
-    this.z=this.x+this.y;
-    this.vt=a.length;
     this.createavuong=function() {
-        a.push(new Pixcel(this.x, this.y, "red"));
-        a.push(new Pixcel(this.x, this.y + 30, "red"));
-        a.push(new Pixcel(this.x + 30, this.y, "red"));
-        a.push(new Pixcel(this.x + 30, this.y + 30, "red"));
+        a[0]=new Pixcel(this.x, this.y, "red");
+        a[1]=new Pixcel(this.x, this.y + 30, "red");
+        a[2]=new Pixcel(this.x + 30, this.y, "red");
+        a[3]=new Pixcel(this.x + 30, this.y + 30, "red");
     }
     this.drawvuong=function () {
-        a[this.vt].createapixcel();
-        a[this.vt+1].createapixcel();
-        a[this.vt+2].createapixcel();
-        a[this.vt+3].createapixcel();
+        a[0].createapixcel();
+        a[1].createapixcel();
+        a[2].createapixcel();
+        a[3].createapixcel();
     }
     this.cleardraw=function () {
-        a[this.vt].clearapixcel();
-        a[this.vt+1].clearapixcel();
-        a[this.vt+2].clearapixcel();
-        a[this.vt+3].clearapixcel();
+        a[0].clearapixcel();
+        a[1].clearapixcel();
+        a[2].clearapixcel();
+        a[3].clearapixcel();
     }
     this.rowi=function () {
         this.y+=30;
-        a[this.vt].y+=30;
-        a[this.vt+1].y+=30;
-        a[this.vt+2].y+=30;
-        a[this.vt+3].y+=30;
+        a[0].y+=30;
+        a[1].y+=30;
+        a[2].y+=30;
+        a[3].y+=30;
     }
     this.moveleft=function () {
         this.x-=30;
-        a[this.vt].x-=30;
-        a[this.vt+1].x-=30;
-        a[this.vt+2].x-=30;
-        a[this.vt+3].x-=30;
+        a[0].x-=30;
+        a[1].x-=30;
+        a[2].x-=30;
+        a[3].x-=30;
     }
     this.moveright=function () {
         this.x+=30;
-        a[this.vt].x+=30;
-        a[this.vt+1].x+=30;
-        a[this.vt+2].x+=30;
-        a[this.vt+3].x+=30;
+        a[0].x+=30;
+        a[1].x+=30;
+        a[2].x+=30;
+        a[3].x+=30;
     }
 
 }
+an.bandau();
 a1[0]=new vuong(0,0);//                                Doan
 a1[0].createavuong();//
 setInterval(function (){//                       nay
+    if(a1[0].y<((an.maxday[(a1[0].x/30)]-2)*30)){         //a1[0].y<((an.maxday[(a1[0].x/30)]-2)*30 điều kiện tạm thôi, cần phải sửa.
     a1[0].cleardraw();//                                 cho
     a1[0].rowi() //
-    a1[0].drawvuong();//                                   chay
-    console.log(a1[0].z);//
-},1000);//                                           thu
+    a1[0].drawvuong()
+        console.log('ddd')
+    }else {
+        an.mangan[a[0].y/30][a[0].x/30]=1
+        an.mangan[a[1].y/30][a[1].x/30]=1
+        an.mangan[a[2].y/30][a[2].x/30]=1
+        an.mangan[a[3].y/30][a[3].x/30]=1
+        an.setmaxday();
+        a1[0]=new vuong(0,0)
+        a1[0].createavuong();
+    }                               //                                   chay
+},500);//                                           thu
 
-function hinh(event) {
+function Controls(event) {
     switch (event.code) {
         case "KeyA":
             if(a1[0].x>=30){
@@ -92,6 +128,6 @@ function hinh(event) {
     }
 
 }
-document.addEventListener("keydown",hinh)
+document.addEventListener("keydown",Controls)
 
 // Ý tưởng: cho tất cả các pixcel cuối cùng vaò 1 mảng 2 chiều để tính cách sụt hố ăn điểm.
